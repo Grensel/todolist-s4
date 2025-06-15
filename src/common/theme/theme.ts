b6a3cd0d22810399c6eb654b30538095d3da84ce
Grensel <sergeiodinochenko@mail.ru>
2025-06-16 00:33:48 +0300

    feat: change theme

--- src/common/theme/theme.ts
+++ src/common/theme/theme.ts
@@ -8,6 +8,10 @@ export const getTheme = (themeMode: ThemeMode) => {
       primary: {
         main: "rgb(8, 81, 164)",
       },
+      background: {
+        default: themeMode === "dark" ? "rgb(20, 20, 20)" : "rgb(228, 240, 255)", // Цвет фона
+        paper: themeMode === "dark" ? "#1e1e1e" : "rgb(250, 252, 255)", // Цвет бумаги
+      },
     },
   })
 }
